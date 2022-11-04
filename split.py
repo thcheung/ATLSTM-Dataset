67f4a4eec5626a4e0e685d522cf7fbae542b9d4d
thcheung <tsun-hin.cheung@connect.polyu.hk>
2022-11-04 17:46:48 +0800

    naming csv

--- split.py
+++ split.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.utils import resample
 
-df = pd.read_csv('./result.csv')
+df = pd.read_csv('./full.csv')
 #df = df.drop_duplicates(subset=['sentence'],keep='first')
 #df = df[df.sentence != None]
 """
@@ -35,5 +35,5 @@ test.to_csv('./dataset/test.csv',index=False)
 train, test = train_test_split(df, train_size=0.8)
 train.to_csv('./train.csv', index=False)
 dev, test = train_test_split(test, train_size=0.50)
-dev.to_csv('./dev.csv', index=False)
+dev.to_csv('./val.csv', index=False)
 test.to_csv('./test.csv', index=False)
